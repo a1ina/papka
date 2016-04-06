8a9945bde4fc8f5395d6eb4a2adde71e0a1a856b
a1ina <alina.ma1ina@mail.ru>
2016-04-06 12:13:39 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<title><?php bloginfo('name'); ?><?php wp_title( ); ?></title>
+
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<script async src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script async src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js "></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
